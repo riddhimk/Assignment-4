e0218ce320b4f1d8f94e5c41ede508e68921b331
Riddhim Kawdia <riddhim10000@gmail.com>
2025-03-04 19:50:32 +0530

    Created Main.java
    - Initialized a new deck
    - Imported Scanner to take choice input
    - Created a variable "choice" to store user input

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Main {
+    
+    public static void main(String args[]) {
+        
+        Deck deck = new Deck();
+        Scanner scan = new Scanner(System.in);
+        int choice;
+        
