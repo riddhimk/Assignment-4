7773d65888668fc431eb35bb332159dd1e072279
Riddhim Kawdia <riddhim10000@gmail.com>
2025-03-04 19:50:50 +0530

    Created the menu contents

--- Main.java
+++ Main.java
@@ -8,3 +8,18 @@ class Main {
         Scanner scan = new Scanner(System.in);
         int choice;
         
+        do {
+
+			System.out.println("\n===== MENU =====");
+			System.out.println("1. Create Deck");
+			System.out.println("2. Print Deck");
+			System.out.println("3. Print a Card");
+			System.out.println("4. Check if Two Cards are of the Same Suit");
+			System.out.println("5. Compare Two Cards (Same Rank)");
+			System.out.println("6. Find a Card");
+			System.out.println("7. Deal 5 Random Cards");
+			System.out.println("8. Shuffle Deck");
+			System.out.println("9. Exit");
+			System.out.print("Enter your choice: ");
+			choice = scan.nextInt();
+
