51db57552918d4b1f7505d9636aed4b4faf00392
Riddhim Kawdia <riddhim10000@gmail.com>
2025-03-04 19:46:51 +0530

    Updated Card.java

--- Card.java
+++ Card.java
@@ -5,3 +5,14 @@ class Card {
 	String suit;
 	
 	// parameterized contructor
+	public Card(String rank, String suit) {
+		
+		this.rank = rank;
+		this.suit = suit;
+	}
+	
+	// method to display rank and suit of a card
+	public String toString() {
+		return rank+" of "+suit;
+	}
+}
